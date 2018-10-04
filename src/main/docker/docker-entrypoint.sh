0ad70e9f574700293bd09d90960ce7a39cb13f07
graeme <graeme@kilchattanbay.co.uk>
2018-10-04 22:36:11 +0100

    Gradle now builds a docker image

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-if [ "$1" = 'run-game-engine' ]; then
-    java -javaagent:$JAVA_AGENT -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar game-engine.war
-else
-    echo "Unsupported command '$1' - supported commands are 'run-game-engine'"
-fi
+#java -javaagent:$JAVA_AGENT -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar game-engine.war
+java -jar game-engine.war
